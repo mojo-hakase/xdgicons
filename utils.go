3ac8ad6455ad60b2f0a5c854cce41fa4a7cf7f58
mojo <github@mojo.hakase.de>
2026-07-23 22:36:40 +0200

    fix default xdg data dirs

--- utils.go
+++ utils.go
@@ -15,12 +15,26 @@ func abs(n int) int {
 }
 
 func GetBaseDirs() (baseDirs []string) {
-	homeDir := os.Getenv("HOME")
-	dataDirs := strings.Split(os.Getenv("XDG_DATA_DIRS"), ":")
+	// Let's construct this and append "icons" to each path:
+	// "${XDG_DATA_HOME:-$HOME/.local/share}:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
+
+	xdgDataDirs := "/usr/local/share:/usr/share"
+	if value := os.Getenv("XDG_DATA_DIRS"); value != "" {
+		xdgDataDirs = value
+	}
+	dataDirs := strings.Split(xdgDataDirs, ":")
+
+	dataHomeDir := ""
+	if value := os.Getenv("XDG_DATA_HOME"); value != "" {
+		dataHomeDir = value
+	} else if value := os.Getenv("HOME"); value != "" {
+		dataHomeDir = path.Join(value, ".local", "share")
+	}
+
 	pixmapDir := "/usr/share/pixmaps"
 
-	if homeDir != "" {
-		baseDirs = append(baseDirs, path.Join(homeDir, ".icons"))
+	if dataHomeDir != "" {
+		baseDirs = append(baseDirs, path.Join(dataHomeDir, "icons"))
 	}
 
 	for _, dataDir := range dataDirs {
